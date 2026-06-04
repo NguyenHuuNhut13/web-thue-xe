683237960d542f42fbad9cb0c31f785eab830500
Nhựt <teamnsg002@gmail.com>
2026-06-04 18:04:45 +0700

    debug: enable verbose php errors and set working directory on Vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,13 @@
 <?php
 
+// Force error reporting on Vercel for diagnostic purposes
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Set working directory to project root
+chdir(__DIR__ . '/../');
+
 // Forward Vercel requests to normal index.php
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 
