ab8589242e971f79a6ab9478bf9d8fd932395b94
Nhựt <teamnsg002@gmail.com>
2026-06-09 19:00:42 +0700

    feat: make Users resource read-only for admin (view-only instead of edit/create)

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources\Users\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Table;
@@ -79,7 +79,7 @@ class UsersTable
                 //
             ])
             ->recordActions([
-                EditAction::make(),
+                ViewAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
